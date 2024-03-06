3f47a6d89d3a51125ab1bad2f8b7da2e386c4895
Javi-Gallego <galgar@gmail.com>
2024-03-06 19:10:33 +0100

    feat: Create Express server with TS and connection to MySQL DB

---
+++ src/server.ts
@@ -0,0 +1,30 @@
+
+import express, { Application } from 'express'
+import 'dotenv/config'
+import { AppDataSource } from './db'
+
+const PORT = process.env.PORT || 4001
+
+const app:Application = express()
+
+app.use(express.json())
+
+app.get("/API/healthy", (req, res) => {
+    res.send("Server is healthy")
+})
+
+const startServer = () => {
+    AppDataSource.initialize()
+    .then(() => {
+        console.log("Database connected")
+
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`)
+        })
+    })
+    .catch((err) => {
+        console.error(err)
+    })
+}
+
+startServer()
