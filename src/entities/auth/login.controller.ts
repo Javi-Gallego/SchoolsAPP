d1c33a714d3b3e9eae086279bb7837694806ff2d
Javi-Gallego <galgar@gmail.com>
2024-04-24 00:10:29 +0200

    feat: finished auth entity

--- src/entities/auth/login.controller.ts
+++ src/entities/auth/login.controller.ts
@@ -1,19 +1,19 @@
 import { Request, Response } from "express"
-import { HttpStatus, ValidationError, handleError } from "../../utils/handleError"
-// import { getLoginService } from "./login.service.js"
+import { ForbiddenError, HttpStatus, NotFoundError, ValidationError, handleError } from "../../utils/handleError"
+import { getLoginService } from "./login.service"
 
 export const login = async (req: Request, res: Response) => {
     try {
-        // const token = await getLoginService(req)
+        const token = await getLoginService(req)
 
         res.status(HttpStatus.OK).json({
             success: true,
             message: "User logged succesfully",
-            // token: token
+            token: token
         })
 
     } catch (error) {
-        if (error instanceof ValidationError) {
+        if (error instanceof ValidationError || error instanceof ForbiddenError || error instanceof NotFoundError) {
             return handleError(res, error.message, HttpStatus.BAD_REQUEST, error.name)
         }
         handleError(res, "Cant log user", HttpStatus.INTERNAL_SERVER_ERROR, "")
---
+++ src/entities/auth/login.service.ts
@@ -0,0 +1,42 @@
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+import { Request } from "express";
+import { ForbiddenError, NotFoundError, ValidationError } from "../../utils/handleError";
+import { findUserByEmail } from "./login.repository";
+
+export const getLoginService = async (req: Request) => {
+
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+        throw new ValidationError("Email and password are required");
+    }
+
+    const user = await findUserByEmail(email);
+
+    if (!user) {
+        throw new NotFoundError("User not found");
+    }
+
+    const passwordMatch = bcrypt.compareSync(password, user.passwordHash);
+
+    if (!passwordMatch) {
+        console.log("Password incorrect")
+        throw new ForbiddenError("Password incorrect");
+    }
+
+    const token = jwt.sign(
+        { 
+            userId: user.id,
+            firstName: user.firstName,
+            profilePhoto: user.profilePhoto,
+            schoolId: user.schoolId,
+        }, 
+        process.env.JWT_SECRET as string, 
+        {
+            expiresIn: 86400,
+        }
+    );
+
+    return token;
+};
