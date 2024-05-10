b33b99c1b3a63cc248af4e680772c194b79a9b83
Javi-Gallego <galgar@gmail.com>
2024-05-10 13:59:24 +0200

    fix: added origin to cors in app.ts

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,7 @@ import cors from "cors";
 
 export const app: Application = express();
 
-app.use(cors());
+app.use(cors({ origin: 'http://localhost:5173' }));
 app.use(express.json());
 app.use("/uploads", express.static("uploads"));
 
